5d113050c20ed623aff96d5944f204e4c3fd2b39
Elepicos <73370863+Elepicos@users.noreply.github.com>
2022-07-18 11:47:20 -0500

    working purge

--- slash_commands.py
+++ slash_commands.py
@@ -12,10 +12,15 @@ class SetupCommands(commands.Cog):
     async def purge(self, ctx, quantity: discord.Option(int), user: discord.Option(discord.User, required = True, default = None)):
         res=""
         if user == None:
-            await ctx.purge(limit=quantity)
+            await ctx.channel.purge(limit=quantity)
             res+=(f"purging {quantity} messages from the channel")
         else:
             await ctx.channel.purge(limit=quantity, check=lambda msg: msg.author == user)
             res+=(f"purging {quantity} messages from {user} in the channel")
         await ctx.respond(res)
+
+    @purge.error
+    async def purge_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.respond("You are not authorized to use this command")
 
